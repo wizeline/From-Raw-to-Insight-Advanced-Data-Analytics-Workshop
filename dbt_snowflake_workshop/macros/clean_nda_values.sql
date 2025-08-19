39edc601bbea3a150bce3f9f3858472212bb08b4
Carlos Razo <carlos.razo.contractor@realtor.com>
2025-08-18 20:04:37 -0600

    Remove unused macros: clean_nda_values, parse_date, validate_percentage, create_anomaly_object

--- dbt_snowflake_workshop/macros/clean_nda_values.sql
+++
@@ -1,4 +0,0 @@
-{% macro clean_nda_values(column_name) %}
-    case when {{ column_name }} = 'NDA' then null else {{ column_name }} end
-{% endmacro %}
-
--- dbt_snowflake_workshop/macros/validate_percentage.sql
+++
@@ -1,6 +0,0 @@
-{% macro validate_percentage(percentage_field) %}
-    case 
-        when {{ percentage_field }} < 0 or {{ percentage_field }} > 100 then null
-        else {{ percentage_field }}
-    end
-{% endmacro %}

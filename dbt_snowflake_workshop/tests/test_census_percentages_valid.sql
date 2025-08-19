88deb1325e4378301e1fd5ee4d12d72960f82e0f
Carlos Razo <carlos.razo.contractor@realtor.com>
2025-08-18 20:02:01 -0600

    Fase 1 Workshop: Remove silver/gold layers and anomaly detection, keep only bronze models

--- dbt_snowflake_workshop/tests/test_census_percentages_valid.sql
+++
@@ -1,9 +0,0 @@
--- Custom test to ensure census percentages are within valid ranges (0-100)
-select *
-from {{ ref('silver_census') }}
-where (percent_housing_crowded < 0 or percent_housing_crowded > 100)
-   or (percent_households_below_poverty < 0 or percent_households_below_poverty > 100)
-   or (percent_unemployed < 0 or percent_unemployed > 100)
-   or (percent_without_high_school < 0 or percent_without_high_school > 100)
-   or (percent_dependent_population < 0 or percent_dependent_population > 100)
-   or (hardship_index < 0 or hardship_index > 100)
--- dbt_snowflake_workshop/tests/test_crime_dates_valid.sql
+++
@@ -1,5 +0,0 @@
--- Custom test to ensure crime incident dates are valid
-select *
-from {{ ref('silver_crime') }}
-where incident_date is not null 
-  and (incident_date < '2000-01-01' or incident_date > current_date())
